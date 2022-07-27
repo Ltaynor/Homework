3c682bebf8adba383387bcda1de198643595b3b8
Ltaynor <larry.taynor@gmail.com>
2022-07-27 18:32:17 -0400

    classwork and folders within

---
+++ class_work_week1/loop.js
@@ -0,0 +1,32 @@
+//for(let i = 0; i < 99; i++){
+//    console.log(i)
+//}
+
+// EVEN numbers 2-98
+
+// for(let i = 2; i < 98; i += 2){
+//     console.log(i)
+// }
+
+// ODD numbers: 1-99
+
+//for(let i = 1; i < 99; i += 2){
+//     console.log(i)
+//}
+/*
+Using the For loop, im creating a variable 
+then have to give the variable a condition, or else without it you will end up with a infinite loop i think because it does not know where
+to stop.
+then incriminting/decrementing it. without the incriment, your varaible will never move 
+then console.log; 
+ */
+
+//start at 81. Stop at 26
+//  for(let i = 81; i > 25; i--){
+//    console.log(i)
+//  }
+ /*starting our variable at 81 and then having our condition stop at 26 by giving it the number 25 */
+
+ for(let i = 49; i <= 72; i++){
+    console.log(i)
+  }
---
+++ class_work_week2/main.js
@@ -0,0 +1,65 @@
+// //function sayhello(name) {
+//  //   console.log(`Hello, ${name}`)
+// //}
+
+// sayhello('Larry')
+
+// function secondSayHello(name) {
+//     return `Hello, ${name}`
+// }
+
+// const result = secondSayHello('Larry');
+//     console.log(result)
+
+// function addTwo(num1, num3) {
+//     return num1 + num3;
+//     }
+//     // parameter is what it is looking for
+//     // argument is the specific value that is held within the parameter 
+    
+//     //Scope
+//     //global scope - Able to be used anywhere 
+
+//     const array = [1, 2, 3];
+//     const number = 12;
+//     const name = 'Larry'
+
+//     function grabOutsideVariable() {
+//         console.log(`hello ${name}`)
+//     }
+
+//     function nestedFunctionParent() {
+//         const age = 2000;
+
+//     }
+
+//     function nestedFunctionChild() {
+//         console.log(age);
+//     }
+
+//     nestedFunctionParent();
+
+//     nestedFunctionChild();
+
+
+//Say hello
+    function sayHello(name) {
+        console.log('Hello World')
+    }
+    sayHello()
+
+// area of a rectangle 
+const recLength = 9
+const recWidth = 4
+    function area(recLength, recWidth) {
+        return recWidth * recLength;
+    }
+area(9, 4)
+
+// number squared 
+    function numberSquared(num1, num2){
+        return 
+      }
+    
+// perimeter of circle
+      
